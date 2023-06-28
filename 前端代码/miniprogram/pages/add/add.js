250addf830631a47e8f71af2e6e5da45ca33987c
qiuAxia <94544435+qiu523@users.noreply.github.com>
2023-06-28 13:13:19 +0800

    发布页面布局

--- 前端代码/miniprogram/pages/add/add.js
+++ 前端代码/miniprogram/pages/add/add.js
@@ -5,9 +5,35 @@ Page({
      * 页面的初始数据
      */
     data: {
-
+        address:""
     },
-
+    getloca(){
+        wx.navigateTo({
+          url: '../map/map',
+        })
+    },
+    getLocation: function() {
+        var that = this;
+        wx.getLocation({
+            type: 'gcj02', // gcj02火星坐标系，用于地图标记点位
+      success (res) { // 获取成功
+        that.setInfo([parseFloat(res.latitude), parseFloat(res.longitude)]) // 设置经纬度信息
+        that.getLocation() // 获取当前位置点
+      },
+      fail (e) { // 获取失败
+        if (e.errMsg.indexOf('auth deny') !== -1) { // 如果是权限拒绝
+          wx.showModal({ // 显示提示
+            content: '你已经拒绝了定位权限，将无法获取到你的位置信息，可以选择前往开启',
+            success (res) {
+              if (res.confirm) { // 确认后
+                wx.openSetting() // 打开设置页，方便用户开启定位
+              }
+            }
+          })
+        }
+      }
+    })
+},
     /**
      * 生命周期函数--监听页面加载
      */
